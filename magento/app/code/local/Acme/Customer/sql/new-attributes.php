aafaa87016b44f06d9b2fa8f63eb8a62f360d689
Mike Pretzlaw <pretzlaw@gmail.com>
2013-05-23 15:05:48 +0200

    Snippet to add new customer attributes in an easy way with an array

--- magento/app/code/local/Acme/Customer/sql/new-attributes.php
+++ magento/app/code/local/Acme/Customer/sql/new-attributes.php
@@ -25,21 +25,38 @@ foreach ($attributes as $entityTypeId => $entity)
 {
     foreach ($entity as $code => $attr)
     {
-        $setup->addAttribute($entityTypeId, $code, $attr);
+        $attribute = $eavConfig->getAttribute($entityTypeId, $code);
 
-        // see: app/code/core/Mage/Customer/sql/customer_setup/mysql4-data-upgrade-1.4.0.0.13-1.4.0.0.14.php:53
-        $attribute = $eavConfig->getAttribute('customer', $code);
-        if (!$attribute) {
+        if (!$attribute)
+        {
+            $setup->addAttribute($entityTypeId, $code, $attr);
+        }
+        else
+        {
+            $setup->updateAttribute($entityTypeId, $code, $attr);
+        }
+
+        // see /htdocs/app/code/core/Mage/Customer/sql/customer_setup/mysql4-data-upgrade-1.4.0.0.13-1.4.0.0.14.php:60
+
+        $attribute = $eavConfig->getAttribute($entityTypeId, $code);
+        if (!$attribute)
+        {
             continue;
         }
-        if (false === ($attribute->getData('is_system') == 1 && $attribute->getData('is_visible') == 0)) {
+
+        if (false === ($attribute->getData('is_system') == 1 && $attribute->getData('is_visible') == 0))
+        {
             $usedInForms = $defaultUsedInForms;
-            if (!empty($data['adminhtml_only'])) {
+            if (!empty($data['adminhtml_only']))
+            {
                 $usedInForms = array('adminhtml_customer');
-            } else {
+            }
+            else
+            {
                 $usedInForms[] = 'adminhtml_customer';
             }
-            if (!empty($data['admin_checkout'])) {
+            if (!empty($data['admin_checkout']))
+            {
                 $usedInForms[] = 'adminhtml_checkout';
             }
             $attribute->setData('used_in_forms', $usedInForms);
