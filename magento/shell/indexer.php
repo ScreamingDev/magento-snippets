1821458ff12f1235c5d71ab1debcdd82c092097d
Mike Pretzlaw <pretzlaw@gmail.com>
2013-07-16 15:19:49 +0200

    Create indexer.php
    Possible values for indexer

---
+++ magento/shell/indexer.php
@@ -0,0 +1,15 @@
+# Possible values for the indexer
+
+`php indexer.php --reindex <index>`
+
+Index can be
+
+ - catalog_product_attribute
+ - catalog_product_price
+ - catalog_url
+ - catalog_product_flat
+ - catalog_category_flat
+ - catalog_category_product
+ - catalogsearch_fulltext
+ - cataloginventory_stock
+ - tag_summary
